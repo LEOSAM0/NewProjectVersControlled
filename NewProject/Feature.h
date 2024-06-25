0ce5075bb9175e0f2bed9ef2b57148781f3b0a97
LeoSam <truba666@mail.ru>
2024-06-25 12:08:19 +0500

    created in Feature.h Constructor, Destructor; protected:  int AdditionOfFeature; private: int NumberOfFeatures, std::string NameOfFeature

--- NewProject/Feature.h
+++ NewProject/Feature.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <iostream>
+
+class Feature
+{
+public:
+	Feature();
+
+	~Feature();
+
+protected:
+	int AdditionOfFeature;
+
+private:
+	int NumberOfFeatures;
+	std::string NameOfFeature;
+};
